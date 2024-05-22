4efacbe8be3fc16428fc998d2664c0040979c943
Gabriela Oroz <gabriela.oroz@fer.hr>
2024-05-22 14:25:02 +0200

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/shared/Fotter";
+import Header from "@/components/shared/Header";
+import { Text } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Text>Home</Text>
+      <Footer />
+    </>
+  );
+}
